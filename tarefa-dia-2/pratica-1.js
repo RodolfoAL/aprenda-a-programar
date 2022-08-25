948b025218586dacb72974ed8c4f3a8dddf30476
Rodolfo Lisboa <rodolfoassislisboa@gmail.com>
2022-08-25 12:54:43 -0300

    Desafio opcional etapa 2

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -8,3 +8,7 @@ console.log(email);
 console.log(profissão);
 console.log(idade);
 console.log(local);
+
+console.log(
+  `Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${local}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissão} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`
+);
