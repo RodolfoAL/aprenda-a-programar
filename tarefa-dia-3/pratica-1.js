32aed624a2bb1570c2a5d857f6989fc238575d50
Rodolfo Lisboa <rodolfoassislisboa@gmail.com>
2022-08-25 17:43:52 -0300

    Práticas 1 e 2

---
+++ tarefa-dia-3/pratica-1.js
@@ -0,0 +1,7 @@
+var n = 9;
+
+var i = 1;
+while (i <= n) {
+  console.log(i);
+  i++;
+}
---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,7 @@
+var n = 9;
+
+var i = 1;
+while (i <= n + 1) {
+  console.log(`${n} x ${i} = ` + n * i);
+  i++;
+}
